997bdbab965d97e8c3c2e6dd7f38d2b4cf7604ab
vfr750y <th91843@proton.me>
2025-08-20 17:51:55 +1200

    add readme

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -3,5 +3,15 @@ pragma solidity ^0.8.19;
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
-    constructor() ERC721("Dogie", "DOG") {}
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("VeeFour", "VFR") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNtf() public {}
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {}
 }
