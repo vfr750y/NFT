7b9d03a29a14c325b94fc331b365e96b174f833b
vfr750y <th91843@proton.me>
2025-08-21 20:05:42 +1200

    add interactions

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -1,22 +1,26 @@
 // SPDX-License-Identifier:MIT
 
-pragma solidity ^0.8.19
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNFT.sol";
 
 contract MintBasicNft is Script {
-    string public constant VEEFOURHRC = "https://ipfs.io/ipfs/QmQvDNeHPbmwcHHuX9G3b7VwAQV1pbZbtyg8576CxeMr2S";
-    function run() external {
-address mostRecentlyDeployed = DevOpsToos.get_most_recent_deployment(
-    "BasicNft", block.chainid);
-mintNftOnContract(mostRecentlyDeployed);
+    string public constant VEEFOURHRC =
+        "https://ipfs.io/ipfs/QmQvDNeHPbmwcHHuX9G3b7VwAQV1pbZbtyg8576CxeMr2S";
 
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "BasicNft",
+            block.chainid
+        );
+        mintNftOnContract(mostRecentlyDeployed);
     }
-function mintNftOnContract(address contractAddress) public {
-    vm.startBroadcast();
-    BasicNft(contractAddress).mintNft(VEEFOURHRC);
-    vm.stopBroadcast();
-}
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        BasicNft(contractAddress).mintNft(VEEFOURHRC);
+        vm.stopBroadcast();
+    }
 }
