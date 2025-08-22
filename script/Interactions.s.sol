5cdaabd39b7bcdb35d5bad046dac3f4a9460c9c2
vfr750y <th91843@proton.me>
2025-08-22 21:22:23 +1200

    testing mint

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -10,6 +10,17 @@ contract MintBasicNft is Script {
     string public constant VEEFOURHRC =
         "https://ipfs.io/ipfs/QmQvDNeHPbmwcHHuX9G3b7VwAQV1pbZbtyg8576CxeMr2S";
 
+    // string public constant VEEFOURHRC =
+    //    "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    /*  // Hardcode the deployed contract address
+   // address public constant BASIC_NFT_ADDRESS =
+   //     0x5FbDB2315678afecb367f032d93F642f64180aa3; // Replace with actual address
+
+    function run() external {
+        mintNftOnContract(BASIC_NFT_ADDRESS);
+    }
+*/
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
             "BasicNft",
