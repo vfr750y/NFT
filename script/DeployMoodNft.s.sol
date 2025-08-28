55c95d8e485b5d6b2f00604c22a3dd50df3fd74d
vfr750y <th91843@proton.me>
2025-08-28 12:33:41 +1200

    add test

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -1,12 +1,22 @@
 // SPDX-License-Identifer:MIT
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
 import {Base64} from "openzeppelin-contracts/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
-    function run() external returns (MoodNft) {}
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./images/sad.svg");
+        string memory happySvg = vm.readFile("./images/happy.svg");
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(
+            svgToImageURI(sadSvg),
+            svgToImageURI(happySvg)
+        );
+        vm.stopBroadcast();
+        return moodNft;
+    }
 
     function svgToImageURI(
         string memory svg
