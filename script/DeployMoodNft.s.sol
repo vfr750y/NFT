257a3a8a1fd23c5e04131e6523080bd0e7ea33b9
vfr750y <th91843@proton.me>
2025-08-30 13:10:19 +1200

    test deploy

--- script/DeployMoodNft.s.sol
+++ script/DeployMoodNft.s.sol
@@ -1,9 +1,9 @@
-// SPDX-License-Identifer:MIT
+// SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
-import {Base64} from "openzeppelin-contracts/contracts/utils/Base64.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract DeployMoodNft is Script {
     function run() external returns (MoodNft) {
@@ -21,7 +21,7 @@ contract DeployMoodNft is Script {
     function svgToImageURI(
         string memory svg
     ) public pure returns (string memory) {
-        string memory baseURL = "data:/image/svg+xml;base64,";
+        string memory baseURL = "data:image/svg+xml;base64,";
         string memory svgBase64Encoded = Base64.encode(
             bytes(string(abi.encodePacked(svg)))
         );
--- test/DeployMoodNftTest.t.sol
+++ test/DeployMoodNftTest.t.sol
@@ -1,4 +1,4 @@
-// SPDX-LicenseIdentifier:MIT
+// SPDX-License-Identifier:MIT
 pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
@@ -13,7 +13,7 @@ contract DeployMoodNftTest is Test {
 
     function testConvertSvgToUri() public view {
         string
-            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCI+Cjx0ZXh0IHg9IjAiIHk9IjE1IiBmaWxsPSJibGFjayI+SGkhIFlvdXIgYnJvd3NlciBkZWNvZGVkIHRoaXM8L3RleHQ+Cjwvc3ZnPg==";
+            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCI+PHRleHQgeD0iMCIgeT0iMTUiIGZpbGw9ImJsYWNrIj5IaSEgWW91ciBicm93c2VyIGRlY29kZWQgdGhpczwvdGV4dD48L3N2Zz4=";
         string
             memory svg = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" height="500"><text x="0" y="15" fill="black">Hi! Your browser decoded this</text></svg>';
         string memory actualUri = deployer.svgToImageURI(svg);
